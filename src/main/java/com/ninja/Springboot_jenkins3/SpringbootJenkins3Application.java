6c7ba69b2b811cff8a7c4d45effd84538149492f
Aman Akhtar <amanakhtar0705@gmail.com>
2025-05-01 17:08:23 +0530

    jar file cimmiting

--- src/main/java/com/ninja/Springboot_jenkins3/SpringbootJenkins3Application.java
+++ src/main/java/com/ninja/Springboot_jenkins3/SpringbootJenkins3Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// hello springboot 
 public class SpringbootJenkins3Application {
 
 	public static void main(String[] args) {
