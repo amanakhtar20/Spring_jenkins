83b702174742d4a0080efb11cf2ba50c3e31bcd1
Aman Akhtar <amanakhtar0705@gmail.com>
2025-05-01 17:22:05 +0530

    added jar file

--- src/main/java/com/ninja/Springboot_jenkins3/SpringbootJenkins3Application.java
+++ src/main/java/com/ninja/Springboot_jenkins3/SpringbootJenkins3Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-// hello springboot 
+// hello
 public class SpringbootJenkins3Application {
 
 	public static void main(String[] args) {
